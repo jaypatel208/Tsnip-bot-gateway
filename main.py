9856d205756ea7d6e8b33a2e9c32aaf832f79143
Jay Patel <jay037493@gmail.com>
2025-07-06 13:00:37 +0530

    Use replit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import discord
 import os
+import discord
 from dotenv import load_dotenv
+from keep_alive import keep_alive
 
-# Load environment variables from .env file
 load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+keep_alive()  # start the web server
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -16,4 +17,4 @@ async def on_ready():
     print(f"Tsnip is online: {client.user}")
 
 
-client.run(DISCORD_BOT_TOKEN)
+client.run(TOKEN)
